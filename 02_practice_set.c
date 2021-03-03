d1833e9904c04a0e97e70372aac3c59d01d72a95
Amey Joshi <ameyjoshi2001rj@gmail.com>
2021-03-03 17:18:42 +0530

    02_Practice set for C

---
+++ 02_practice_set.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+int num;
+printf("Enter the number \n");
+scanf("%d",&num);
+printf("Divisibilty test returns: %d\n",num%97);
+
+
+int x = 2,y = 3,z=3,k=1;
+int result = 3 * x/y-z+k;
+printf("The value %d",result);
+return 0;
+}
+  

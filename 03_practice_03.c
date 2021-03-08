66147a8c38beccd2925f7807cf627d4a871b6008
Amey Joshi <ameyjoshi2001rj@gmail.com>
2021-03-08 10:37:15 +0530

    New Changes

--- 03_practice_03.c
+++ 03_practice_03.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(){
-int a ,b ,c ,d;
+int a ,b ,c ;
 
 printf("Enter your First number : \n");
 scanf("%d",&a);
@@ -12,25 +12,16 @@ scanf("%d",&b);
 printf("Enter your Third number : \n");
 scanf("%d",&c);
 
-printf("Enter your Fourth number : \n");
-scanf("%d",&d);
-
-if(a>b && a>c && a>d){
-     printf("The First number you entered is the biggest number.");
+if(a>b && a>c ){
+     printf("The First number you entered is the greatest number.");
 }
-if(b>a && b>c && b>d){
-     printf("The Second number you entered is the biggest number.");
+if(b>a && b>c){
+     printf("The Second number you entered is the greatest number.");
 }
 
-if(c>a && c>b && c>d){
-     printf("The Third number you entered is the biggest number.");
+if(c>a && c>b ){
+     printf("The Third number you entered is the greatest number.");
 }
-
-if(d>a && d>c && d>b){
-     printf("The Fourth number you entered is the biggest number.");
-}
-
-
 return 0;
 
 }
